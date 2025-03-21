365af5872cf89a4fb5a2709e00c7f90bc2302921
this override <n4yor93@hotmail.fr>
2025-03-21 09:13:02 +0100

    change path name

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,9 @@ use controllers::pwr_controller::PwrController;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Créer le service de gestion d'alimentation
     let pwr_service = PwrService::new();
-    
-    // Créer le contrôleur avec le socket et le service
-    let socket_path = Path::new("/tmp/smx-iam-service");
+    let socket_path = Path::new("/run/pwrsvc.sock");
     let controller = PwrController::new(socket_path, pwr_service);
-    
-    // Démarrer le contrôleur
     controller.start().await?;
     
     Ok(())
